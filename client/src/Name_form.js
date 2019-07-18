28f9b2a3ce4f734a9e2cd209023070a339d0c909
Aidan <aidanmro@gmail.com>
2019-07-18 14:56:12 -0400

    added styling and positioning to landing page

--- client/src/Name_form.js
+++ client/src/Name_form.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react';
   class NameForm extends Component{
     render(){
       return(
-        <div className="Name-form-title">
-        Name:
-        <input class="input" type="text" placeholder="Enter your name"></input>
+        <div className="columns is-mobile is-centered">
+          <span className="Name-form-title">
+          </span>
+          <form classname="name-form control">
+            <p>
+              <input class="input" type="text" placeholder="Enter your name">
+              </input>
+            </p>
+          </form>
         </div>
       );
     };
--- client/src/Room_code_form.js
+++ client/src/Room_code_form.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react';
 class RoomCodeForm extends Component{
   render(){
     return( 
-      <div className="room-code-title">
-        Room code:
-        <input class="input" type="text" placeholder="Enter 4-digit room code"></input>
+      <div className="room-code columns is-mobile is-centered">
+        <span className="room-code-title">
+        </span>
+        <form classname="room-code-control control">
+          <p className="control">
+            <input class="input" type="text" placeholder="Enter 4-digit room code">
+            </input>
+          </p>
+        </form>
         </div>
     );
   };
