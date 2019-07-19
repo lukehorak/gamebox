d3fb0f760dc93f1789ad925add24932c5b956e5f
Cher <red_phoenix417@yahoo.com>
2019-07-19 11:42:35 -0400

    Landing page - Create a Page component

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // TODO: Revert back to App
-// import App from './App'
-import CreateGame from './page/CreateGame';
+import App from './App'
+// import CreateGame from './page/CreateGame';
 import './index.css';
 
 ReactDOM.render(
   // TODO: Revert back to App
-  // < App /> ,
-  < CreateGame /> ,
+  < App /> ,
+  // < CreateGame /> ,
   document.getElementById('root')
 );
