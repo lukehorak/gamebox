7dc3821a996f0b54bbd3ea452dfa2254efec6a8e
Cher <red_phoenix417@yahoo.com>
2019-07-19 15:18:40 -0400

    Created Faker Game page

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // TODO: Revert back to App
-import App from './component/App'
+// import App from './component/App'
+import Faker from './component/Faker'
 import './stylesheets/index.css';
 
 ReactDOM.render(
   // TODO: Revert back to App
-  < App /> ,
+  // < App /> ,
+  < Faker /> ,
   document.getElementById('root')
 );
