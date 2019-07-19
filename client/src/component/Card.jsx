2ca9751756018aaa432d7e36388e5d0538b8cb82
Lukas Horak <lukasbhorak@gmail.com>
2019-07-19 14:22:09 -0400

    Merged changes resolving issue #14

---
+++ client/src/component/Card.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import '../stylesheets/App.css';
+import 'bulma/css/bulma.css'
+
+class Card extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+
+    return (
+      <div className="column card">
+        <div className={this.props.cardClass}>
+          <div className="card-image">
+            <figure className="image is-4by5">
+              <img src={this.props.imageSource} alt="Placeholder image" />
+            </figure>
+          </div>
+          <div className="card-content">
+            <p className="title">{this.props.gameTitle}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card; 
