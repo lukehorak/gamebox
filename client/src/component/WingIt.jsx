f97972914a46f05bf9333a3d188b638b8a4919a9
Cher <red_phoenix417@yahoo.com>
2019-07-20 22:57:28 -0400

    deleted FakeDB and made number.js file under util folder to export random generator function

--- client/src/component/WingIt.jsx
+++ client/src/component/WingIt.jsx
@@ -4,6 +4,7 @@ import Card from './../component/Card';
 import db from './FakeDb';
 import Nav from './Nav';
 import '../stylesheets/wingit.css';
+import {getRandomNumber} from '../util/numbers';
 
 // Testing if random number is generated
 // console.log(`Random number is ${db.getRandomNumber()}`);
@@ -13,14 +14,14 @@ class WingIt extends Component {
     super(props)
     this.state = {
       players: [
-        { name: 'Cher', playerId: 1},
-        { name: 'Lukas', playerId: 2},
-        { name: 'Aiden', playerId: 3}
+        { name: 'Cher', playerId: 1 },
+        { name: 'Lukas', playerId: 2 },
+        { name: 'Aiden', playerId: 3 }
       ]
     }
   }
   render() {
-    const randomRoomId = db.getRandomNumber();
+    const randomRoomId = getRandomNumber();
     const players = this.state.players.map(function (player) {
       return (
         <li key={player.playerId} className="my-player-list-item">
@@ -29,15 +30,14 @@ class WingIt extends Component {
       );
     });
 
+
     // db.createNewRoom(randomRoomId, randomRoomName)
 
     return (
       <div>
         < Nav />
         <div className="wingit-main-container">
-          <ul>
-            {players}
-          </ul>
+
 
           <div className="generated-room-code">
             Room Code: {randomRoomId}
@@ -46,21 +46,24 @@ class WingIt extends Component {
           <form action="/wherever-handling-form-page" method="post">
             <div className="player-field">
               <label htmlFor="name">Enter Player Name:</label>
-              <input type="text" id="name" name="user_name"/>
+              <input type="text" id="name" name="user_name" />
             </div>
           </form>
 
           <img src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Ftrophy01.np.community.playstation.net%2Ftrophy%2Fnp%2FNPWR11790_00_01BCC972BD625278DB5379A92D7402F7DD436EA73B%2F809062AAAF2C879E0D3F5FE30421DCBFB37F14A9.PNG&f=1" />
 
-
+          <ul>
+            {players}
+          </ul>
 
 
 
 
         </div>
       </div>
+
           );
-        }
-      }
-      
+  }
+}
+
 export default WingIt;
