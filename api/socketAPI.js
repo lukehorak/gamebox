2ed34b63c8eb9be6ff907c8dbfab5bc411f0baf2
Lukas Horak <lukasbhorak@gmail.com>
2019-07-22 22:50:33 -0400

    implemented new player joinig room

--- api/socketAPI.js
+++ api/socketAPI.js
@@ -14,32 +14,51 @@ io.on('connection', function(socket){
 
     socket.on('new-game', function(data){
         io.game = new Game();
-        socket.emit('room-code', io.game.roomCode);
+        // Create/Join Room, send roomCode to client
         socket.join(io.game.roomCode);
+        socket.emit('room-code', io.game.roomCode);
+        // Create host player, add them to game, and set this socket as the host
         socket.username = data.username;
         io.game.addPlayerByName(socket.username);
         console.log(`Creating game room with room code ${io.game.roomCode} and joining room as ${socket.username}`);
         socket.isHost = true;
         console.log(`${socket.username} is now the host of game ${io.game.roomCode}`)
+
         console.log(JSON.stringify(io.game.players));
-        socket.join(io.game.roomCode);
     })
 
     socket.on('new-player', function(data) {
-        try{
-            io.game.addPlayerByName(data.username);
-            console.log(`Adding ${data.username} to game ${io.game.roomCode}`);
-            console.log(JSON.stringify(io.game.players));
-            socketApi.sendPlayerList(io.game.roomCode)
+        const room = io.sockets.adapter.rooms[data.roomCode];
+
+        if (room){
+            try{
+                
+                io.game.addPlayerByName(data.username);
+                console.log(`Adding ${data.username} to game ${data.roomCode}`);
+    
+                socketApi.sendPlayerList(data.roomCode)
+            }
+            catch(e){
+                console.warn(e);
+            }
         }
-        catch(e){
-            console.warn(e);
+        else{
+            console.warn(`Unable to add user to room, as room with code '${data.roomCode}' room does not exist!`)
+            socket.emit('empty-room', {error: `Room with code '${data.roomCode}' room does not exist!`})
         }
     })
 
     // GURU test
-    socket.on('need-an-adult', function(data){
+    socket.on('need-an-adult', () => {
         socket.emit('guru', {msg: 'I AM AN ADULT', imgSrc: "https://camo.derpicdn.net/12f6f866643214d0ad51265bd10bfbdebc5765b9?url=http%3A%2F%2Fi4.ytimg.com%2Fvi%2FW9krnrEF0nI%2Fmqdefault.jpg"});
+    })
+
+    socket.on('toggle-gohan', () => {
+        socket.emit('toggle-gohan');
+    })
+
+    socket.on('start-game', () => {
+        console.log('starting game');
     })
 });
 
@@ -48,7 +67,17 @@ socketApi.sendNotification = function() {
 }
 
 socketApi.sendPlayerList = function(roomCode) {
-    io.sockets.in(roomCode).emit('send-players', { players: JSON.stringify(io.game.players) })
+    const players = io.of('/').in(roomCode).clients;
+    console.log(roomCode);
+    io.to(roomCode).emit('send-players', { players: players });
+}
+
+socketApi.toggleGohan = () => {
+    io.sockets.emit('toggle-gohan', 'toggling gohan');
+}
+
+socketApi.findGame = (roomCode) => {
+    //io.sockets.
 }
 
 socketApi.newPlayer = function(roomCode) {
