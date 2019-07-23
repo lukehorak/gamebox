abebc5780fc1cd858c013d4cd352d9f986a278fa
Lukas Horak <lukasbhorak@gmail.com>
2019-07-23 13:53:38 -0400

    controlling game phases via socket messages

--- client/src/component/prototyping/SocketTest.jsx
+++ client/src/component/prototyping/SocketTest.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import INeedAnAdult from './INeedAnAdult';
 import socketIOClient from 'socket.io-client';
 
 class SocketTest extends Component {
   constructor() {
     super();
     this.state = {
-      response: false,
-      image: "https://i.ytimg.com/vi/kscG_gs2BOc/hqdefault.jpg",
       endpoint: "http://localhost:9000",
       roomcode: false,
-      hideGohan: true
+      phase: 0
     }
   }
 
@@ -20,53 +17,52 @@ class SocketTest extends Component {
     const { endpoint } = this.state;
     this.socket = socketIOClient(endpoint);
     // Message Handling
-    this.socket.on('gohan', data => this.setState({ response: data.msg, image: data.imgSrc }));
-    this.socket.on('guru', data => this.setState({ response: data.msg, image: data.imgSrc }));
-    this.socket.on('send-players', data => console.log(data.players))
-    this.socket.on('toggle-gohan', data => this.setState({ hideGohan: !this.state.hideGohan }))
-    this.socket.on('empty-room', data => {alert(data.error)})
+    this.socket.on('send-players', data => console.log(data.players));
+    this.socket.on('empty-room', data => {alert(data.error)});
+
+    this.socket.on('give-player', data => {console.log(data)});
+
+    this.socket.on('give-all-players', data => {console.log(data)})
+
     this.socket.on('room-code', data => {
       this.setState({ roomcode: data });
       this.socket.emit('join', data)
     });
+
+    this.socket.on('phase-change', data => {
+      console.log('CHANGING PHASE!')
+      this.setState({ phase: data.phase })
+    })
   }
 
-  sendMessageToServer = () => {
-    this.socket.emit('need-an-adult', {text: 'i need an adult'});
-  }
-
-  createGame = () => {
-    this.socket.emit('new-game', {username: "Goku"});
+  createGame = (e) => {
+    e.preventDefault();
+    const { username } = e.target.elements 
+    this.socket.emit('new-game', {username: username.value});
   }
 
   submitUser = (e) => {
     e.preventDefault();
     const { username, roomcode } = e.target.elements;
-    console.log("username ==> ", username.value)
     this.socket.emit('new-player', {username: username.value, roomCode: roomcode.value})
   }
 
-  toggleGohan = () => {
-    this.socket.emit('toggle-gohan', 'toggling gohan');
-  }
-
   startGame = () => {
-    this.socket.emit('start-game')
+    console.log('starting game')
+    this.socket.emit('start-game', {code: this.state.roomcode});
   }
 
   render() {
     // get vars from state to render
-    const { response, image, roomcode, hideGohan } = this.state;
+    const { roomcode } = this.state;
     return (
       <div style={{ textAlign: "center" }}>
         {/* toggling component by server message as a demo */}
-        <button onClick={this.toggleGohan}>Toggle gohan</button>
-        {/* basic two-way messaging test */}
-        { !hideGohan && <INeedAnAdult response={response} image={image} askForAdult={this.sendMessageToServer}/>}
         {/*getting room code from game */}
-
-        <hr />
-        <button style={{ fontSize: "large" }} onClick={this.createGame}>Create Game</button>
+        <form onSubmit={this.createGame}>
+          <input name="username" placeholder="Enter your username"></input>
+          <button style={{ fontSize: "large" }} >Create Game</button>
+        </form>
         <p>room code: {roomcode}</p>
 
         <hr />
@@ -79,6 +75,10 @@ class SocketTest extends Component {
         <hr />
 
         <button onClick={this.startGame}>Start Game</button>
+
+        <hr/>
+        <button onClick={() => this.socket.emit('get-all-players', {roomCode: this.state.roomcode})}>Log all players</button>
+        <p>Current Phase: {this.state.phase}</p>
       </div>
     );
   }
