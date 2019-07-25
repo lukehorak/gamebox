12e8dbd8f9c9022a7619bdfc29fa50910fa33e5a
Aidan <aidanmro@gmail.com>
2019-07-25 12:54:25 -0400

    added child components for the round results

---
+++ client/src/component/wingit/FakersCaught.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+  class FakersCaught extends Component{
+    render(){
+      return(
+        <div className="caught-the-faker-container">
+          <h1 className="caught-the-faker">
+            You caught the faker! 
+          </h1>
+        </div>
+      );
+    };
+  };
+
+
+export default FakersCaught;
