1826e2d822b2ddf0eb44f986c73c490975c83277
Cher <red_phoenix417@yahoo.com>
2019-07-25 21:32:46 -0400

    formatted home page, removed join and create buttons, have the cards show up

--- client/src/component/home/Home.jsx
+++ client/src/component/home/Home.jsx
@@ -4,6 +4,8 @@ import Nav from './Nav';
 import CreateButton from './Create_game_button';
 import JoinButton from  './Join_game_button';
 import About from '../home/About';
+import Card from './Card';
+
 
 
 //TODO -functional burger, form anime
@@ -14,10 +16,60 @@ class Home extends Component {
       <div className="main-div">
         <Nav/>
         <div id="main-wrapper">
-          <div id="button-container">
-            <JoinButton/>
-            <CreateButton/>
+
+
+
+          
+          <div id="card-container">
+
+
+            <div className="game-container">
+              <Card
+                cardClass="wingItCard"
+                imageSource="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Ftrophy01.np.community.playstation.net%2Ftrophy%2Fnp%2FNPWR11790_00_01BCC972BD625278DB5379A92D7402F7DD436EA73B%2F809062AAAF2C879E0D3F5FE30421DCBFB37F14A9.PNG&f=1"
+                gameTitle="Wing it!"
+              />
+
+              <Card
+                cardClass="typist"
+                imageSource="https://payload.cargocollective.com/1/1/50182/605229/typeordie-01_900.jpg"
+                gameTitle="The Typist"
+              />
+
+            </div>
+
+
+            <div className="game-container">
+              <span className="card-wrapper">
+                <Card
+                  cardClass="close-enough"
+                  imageSource="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2F736x%2F4e%2F52%2F74%2F4e5274ae468d4970a7e3268269a5f8af.jpg&amp;f=1"
+                  gameTitle="Close enough!"
+                />
+              </span>
+
+              <Card
+                cardClass="whereAmI"
+                imageSource="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F33%2F94%2F0d%2F33940d51edd503c1997c17d48b3d0fca--medieval-map-medieval-party.jpg&f=1" alt="Placeholder image"
+                gameTitle="Where Am I?"
+              />
+
+            </div>
           </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         </div>
       </div>
     );
