7834cef4980340e7603b4f433c5ed524d7bd4e8d
Cher <red_phoenix417@yahoo.com>
2019-07-29 21:13:42 -0400

    colored rules and borders

--- client/src/component/wingit/RulesPage.jsx
+++ client/src/component/wingit/RulesPage.jsx
@@ -14,21 +14,21 @@ class RulesPage extends Component {
       
         <p className="par-1">During each round everyone, except one, will be sent some sort of task, for example "Raise your hand if you had breakfast this morning." </p>
 
-        <p>The player who didn't get the task is the one who will have to "wing it", and his/her goal is to try and blend with others without knowing the task themselves. </p>
+        <p className="par-2">The player who didn't get the task is the one who will have to "wing it", and his/her goal is to try and blend with others without knowing the task themselves. </p>
 
-        <p>The other players have to try to figure out who in the room is faking it, and they are given three chances to get it correct. </p>
+        <p className="par-3">The other players have to try to figure out who in the room is faking it, and they are given three chances to get it correct. </p>
           
-        <p>The vote has to be unanimous (minus one, being the faker) for it to count, so discussing the possible faker is encouraged. </p>
+        <p className="par-4">The vote has to be unanimous (minus one, being the faker) for it to count, so discussing the possible faker is encouraged. </p>
       
-        <p>To the wing woman or man, blend in well!</p>
+        <p className="par-5">To the wing woman or man, blend in well!</p>
       
-        <p>To the rest of the players, try to catch the person trying to wing' it!</p>
+        <p className="par-6">To the rest of the players, try to catch the person trying to wing' it!</p>
 
-        <p>HAVE FUN!</p>
+        <p className="par-7">HAVE FUN!</p>
 
         </div>
 
-        {this.props.isHost && <button onClick={this.props.startGame}>Start game!</button>}
+        {this.props.isHost && <button onClick={this.props.startGame} className="rules-start-game-button">Start game!</button>}
           
 
       </div>
